f1509e69f6daba5775130d53162f953d5d293452
mert_elife@hotmail.com <elife@Elifes-Air.fios-router.home>
2021-10-28 22:14:05 -0400

    first message

---
+++ src/syntax_moazzam_git_project/git01.java
@@ -0,0 +1,12 @@
+package syntax_moazzam_git_project;
+
+public class git01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+		
+	}
+
+}
