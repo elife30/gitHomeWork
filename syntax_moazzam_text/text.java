9bfdf1722eac5fc9a14cf870876feaa8be2c7c97
elife <mert_elife@hotmail.com>
2021-11-02 22:04:55 -0400

    all files

---
+++ syntax_moazzam_text/text.java
@@ -0,0 +1,11 @@
+package syntax_moazzam_git_project;
+
+public class text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my love");
+	}
+
+}
---
+++ syntax_moazzam_text/text2.java
@@ -0,0 +1,10 @@
+package syntax_moazzam_git_project;
+
+public class text2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
