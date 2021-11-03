7ce9460f7bff57a43dee11687a0f3baed72b050a
elife <mert_elife@hotmail.com>
2021-11-02 21:47:03 -0400

    beraber test

---
+++ src/syntax_moazzam_git_project/deneme.java
@@ -0,0 +1,11 @@
+package syntax_moazzam_git_project;
+
+public class deneme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1 nov");
+	}
+
+}
